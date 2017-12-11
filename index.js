81a832a88e221e877dd0d8460b781e9b178045c8
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-12-11 13:35:11 -0800

    Code for slow scroll to id using animate and add skill to link navbar

---
+++ index.js
@@ -0,0 +1,7 @@
+$( document ).ready(function(){
+  $(".nav-link").click(function(e) {
+    e.preventDefault();
+    var aid = $(this).attr("href");
+    $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
+});
+});
