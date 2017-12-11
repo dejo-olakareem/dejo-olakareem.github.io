e0cba577cf5b84bed1aa58e0c0e394301ba8cf8b
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-12-11 13:46:10 -0800

    Debug link for index.js not working

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-$( document ).ready(function(){
-  $(".nav-link").click(function(e) {
-    e.preventDefault();
-    var aid = $(this).attr("href");
-    $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
+$( document ).ready(function() {
+    console.log( "ready!" );
 });
-});
+
+
+// $( document ).ready(function(){
+//     console.log("tatoo")
+//   $(".nav-link").click(function(e) {
+//     e.preventDefault();
+//     var aid = $(this).attr("href");
+//     $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
+//   });
+// });
